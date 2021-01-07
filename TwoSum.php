54eed69e10ad4541255dde2efb028c2e6ea9f738
jonathanshi <305277630@qq.com>
2021-01-07 18:37:10 +0800

    two_sum

--- TwoSum.php
+++ TwoSum.php
@@ -5,9 +5,7 @@ class TwoSum
 {
 
     /**
-     * @param Integer[] $nums
-     * @param Integer $target
-     * @return Integer[]
+     * 暴力法
      */
     function twoSum($nums, $target)
     {
@@ -17,9 +15,30 @@ class TwoSum
         You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
         You can return the answer in any order.
+         *
+         * nums = [2,7,11,15], target = 9
 
-        */
+         */
+        $len = count($nums);
+        for($i=0;$i<$len;++$i)
+        {
+            for($j=$i+1;$j<$len-1;$j++)
+            {
+                if($nums[$i] + $nums[$j] == $target)
+                {
+                    return [$i,$j];
+                }
+            }
+        }
 
+    }
+
+    /**
+     * 排序+双指针
+     *
+    */
+    function sortSum($nums,$target)
+    {
 
     }
 
