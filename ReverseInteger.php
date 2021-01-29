85892830fa7225aff801bde1855629a79503ab5c
jonathanshi <305277630@qq.com>
2021-01-29 18:43:07 +0800

    Reverse Integer

--- ReverseInteger.php
+++ ReverseInteger.php
@@ -8,10 +8,13 @@ class ReverseInteger
      * @param Integer $x
      * @return Integer
      *
-     * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+     * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range , then return 0.
      */
     function reverse($x) {
-
+        if( $x < -pow(2,31)  || $x > pow(2,31) -1 )
+        {
+            return 0;
+        }
     }
 
 
