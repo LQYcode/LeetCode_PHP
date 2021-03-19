3cfc0d2f400cc6c046447ef7209406bad0dd2e62
jonathanshi <305277630@qq.com>
2021-03-19 11:33:02 +0800

    Reverse Integer

--- ReverseInteger.php
+++ ReverseInteger.php
@@ -10,46 +10,46 @@ class ReverseInteger
      *
      * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range , then return 0.
      */
-    function reverse($x) {
-        //oversize
-        if( $x < -pow(2,31)  || $x > pow(2,31) -1 )
-        {
-            return 0;
-        }
-        //reverse then remove the last 0
-        $temp  = str_split($x);
-        if(count($temp) > 0 && $temp[0] == 0)
-        {
-            array_shift($temp);
-        }
-        $temp = array_reverse($temp);
-        if ($x < 0)
-        {
-            $res = -intval(implode('',$temp));
-        }
-        else
-        {
-            $res = intval(implode('',$temp));
-        }
-        return $res;
-    }
+//    function reverse($x) {
+//        //oversize
+//        if( $x < -pow(2,31)  || $x > pow(2,31) -1 )
+//        {
+//            return 0;
+//        }
+//        //reverse then remove the last 0
+//        $temp  = str_split($x);
+//        if(count($temp) > 0 && $temp[0] == 0)
+//        {
+//            array_shift($temp);
+//        }
+//        $temp = array_reverse($temp);
+//        if ($x < 0)
+//        {
+//            $res = -intval(implode('',$temp));
+//        }
+//        else
+//        {
+//            $res = intval(implode('',$temp));
+//        }
+//        return $res;
+//    }
 
-    //todo
     function efficientReverse($x)
     {
         $ans = 0;
-        while ($x != 0)
-        {
-            if(($ans * 10) / 10 != $ans)
-            {
-                $ans = 0;
-                break;
-            }
-            $ans = $ans * 10 + $x % 10;
+        while ($x != 0){
+            $pop = $x % 10;
             $x = $x / 10;
+            $x = intval($x);
 
+            if($ans > pow(2,31) / 10 || ($ans == pow(2,31) /10 && $pop > 7) ){
+                return  0;
+            }
+            if($ans < (-pow(2,31)-1) / 10 || ($ans == (-pow(2,31)-1) /10 && $pop < -8) ){
+                return  0;
+            }
+            $ans = $ans * 10 + $pop;
         }
-
         return $ans;
     }
 
@@ -57,4 +57,4 @@ class ReverseInteger
 }
 
 $obj = new ReverseInteger();
-$obj->reverse(0);
+$obj->efficientReverse(123);
